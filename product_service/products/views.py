15a2b9c74adfe52f0187b013959bf5074edf3a71
vairakkanipv <vairakkani@gmail.com>
2026-03-26 12:42:51 +0530

    product viesset added in views.py

--- product_service/products/views.py
+++ product_service/products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework import action
+from rest_framework import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    #order service will call this url to check available stock
+    @action(detail=True, methods=['get'])
+    def check_stock(self, request,pk=None):
+        product = self.get_object()
+        return Response({
+            'product_id': product.id,
+            'name' : product.name,
+            'stock' : product.stock,
+            'available' : product.stock > 0
+        })
+    
