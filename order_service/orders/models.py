8d2f9a5c78a612bd9de85b9559d7264d8baaa3ae
vairakkanipv <vairakkani@gmail.com>
2026-03-26 13:15:40 +0530

    Order service orders maodel added orders table schema created

--- order_service/orders/models.py
+++ order_service/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    STATUS =[
+        ('pending','Pending'),
+        ('confirmed','Confirmed'),
+        ('failed','Failed'),
+    ]
+    product_id = models.IntegerField()
+    quantity   = models.IntegerField()
+    status     = models.CharField(max_length=20, choices=STATUS, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Order #{seld.id}-> Product {self.product_id}- {self.status}"
+    
